a2bbd19919e9a7756c3d101619a55780f19e1256
Luc Chante <luc@pavilion.guinguette.loc>
2013-12-28 00:13:35 +0100

    Source vierge pour l'application Ajout du fichier de configuration de motion

---
+++ RemoteControl/src/univ/avignon/roboid10/RemoteControlActivity.java
@@ -0,0 +1,13 @@
+package univ.avignon.roboid10;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RemoteControlActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_remote_control);
+	}
+}
